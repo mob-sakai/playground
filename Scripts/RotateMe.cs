3259e6260b0630bd22fa9bf36b07ea476d9d1967
mob-sakai <sakai861104@gmail.com>
2023-11-19 00:57:39 +0900

    init

---
+++ Scripts/RotateMe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Coffee.RotateMe
+{
+	public class RotateMe : MonoBehaviour
+	{
+		public float angularVelocity = 10;
+		void Update ()
+		{
+			var rot = Quaternion.AngleAxis (angularVelocity * Time.deltaTime, Vector3.up);
+			transform.localRotation = rot * transform.localRotation;
+			Debug.Log("hogehogehoge");
+			Debug.Log("fugafugas");
+			#if !UNITY_EDITOR
+			Debug.Log("fix bugs")
+			#endif
+		}
+	}
+}
